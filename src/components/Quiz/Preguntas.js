8a86f0113a1f989a983780baf9372fb5896a2689
Jesús Aldair <aldairtusero1@gmail.com>
2024-03-11 16:05:03 -0600

    un 30%

---
+++ src/components/Quiz/Preguntas.js
@@ -0,0 +1,106 @@
+ const preguntas =[
+    {
+        'numero': 1,
+        'pregunta': "¿Cuál es la capital de Francia?",
+        'respuestas': [
+            { 'respuesta1': 'Londres', 'correcta': false },
+            { 'respuesta2': 'París', 'correcta': true },
+            { 'respuesta3': 'Madrid', 'correcta': false },
+            { 'respuesta4': 'Berlín', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 2,
+        'pregunta': "¿En qué año finalizó la Segunda Guerra Mundial?",
+        'respuestas': [
+            { 'respuesta1': '1943', 'correcta': false },
+            { 'respuesta2': '1944', 'correcta': false },
+            { 'respuesta3': '1945', 'correcta': true },
+            { 'respuesta4': '1946', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 3,
+        'pregunta': "¿Quién inventó el primer automóvil?",
+        'respuestas': [
+            { 'respuesta1': 'Karl Benz', 'correcta': true },
+            { 'respuesta2': 'Henry Ford', 'correcta': false },
+            { 'respuesta3': 'Gottlieb Daimler', 'correcta': false },
+            { 'respuesta4': 'Wilhelm Maybach', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 4,
+        'pregunta': "¿Quién descubrió América?",
+        'respuestas': [
+            { 'respuesta1': 'Cristóbal Colón', 'correcta': true },
+            { 'respuesta2': 'Amerigo Vespucci', 'correcta': false },
+            { 'respuesta3': 'Fernando de Magallanes', 'correcta': false },
+            { 'respuesta4': 'Vasco Núñez de Balboa', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 5,
+        'pregunta': "¿Cuál fue el primer equipo de fútbol profesional en México?",
+        'respuestas': [
+            { 'respuesta1': 'Club de Fútbol Pachuca', 'correcta': true },
+            { 'respuesta2': 'Club América', 'correcta': false },
+            { 'respuesta3': 'Club Necaxa', 'correcta': false },
+            { 'respuesta4': 'Club Deportivo Guadalajara', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 6,
+        'pregunta': "¿Cuál es el continente más grande del mundo?",
+        'respuestas': [
+            { 'respuesta1': 'Asia', 'correcta': true },
+            { 'respuesta2': 'África', 'correcta': false },
+            { 'respuesta3': 'América', 'correcta': false },
+            { 'respuesta4': 'Europa', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 7,
+        'pregunta': "¿En qué país habitan más personas en el mundo?",
+        'respuestas': [
+            { 'respuesta1': 'China', 'correcta': true },
+            { 'respuesta2': 'India', 'correcta': false },
+            { 'respuesta3': 'Estados Unidos', 'correcta': false },
+            { 'respuesta4': 'Indonesia', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 8,
+        'pregunta': "¿Cuál es la civilización más antigua del mundo?",
+        'respuestas': [
+            { 'respuesta1': 'Civilización Mesopotámica', 'correcta': false },
+            { 'respuesta2': 'Civilización Egipcia', 'correcta': false },
+            { 'respuesta3': 'Civilización del Valle del Indo', 'correcta': true },
+            { 'respuesta4': 'Civilización China', 'correcta': false },
+        ],
+    },
+
+    {
+        'numero': 9,
+        'pregunta': "¿Quién fue el último emperador del Imperio Romano?",
+        'respuestas': [
+            { 'respuesta1': 'Rómulo Augusto', 'correcta': true },
+            { 'respuesta2': 'Julio César', 'correcta': false },
+            { 'respuesta3': 'Constantino I', 'correcta': false },
+            { 'respuesta4': 'Nerón', 'correcta': false },
+        ],
+    }
+    
+    
+
+]; 
+
+
+export default preguntas;
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,7 +1,99 @@
-﻿import React from 'react'
+﻿import React, { useEffect, useState } from 'react';
+import { Radio, Space, Button } from 'antd';
+import { Card } from 'antd';
+import logo from '../../img/Francia.jpg';
+import preguntas  from './Preguntas';
+
 
 export default function Quiz() {
+
+
+  const { Meta } = Card;
+
+  
+  const [value, setValue] = useState(1);
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const [indicePregunta, setIndicePregunta] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  let indice = 0;
+
+  useEffect(() => {
+    console.log('JEJEJE ', preguntas[0]?.respuestas[0]); // Usando el operador de opcionalidad (?.)
+    const intervalId = setInterval(() => {
+      console.log('Cambio de pregunta: ', preguntas[indice]);
+      setIndicePregunta(preguntas[indice]);
+      setLoading(true);
+      if (indice === 8) {
+        indice = 0;
+      }
+      indice++;
+    }, 4000);
+  
+    return () => clearInterval(intervalId);
+  }, []);
+  
+
+  let tituloPregunta = indicePregunta.pregunta;
+  let respuesta1 = indicePregunta.respuestas ? indicePregunta.respuestas[0].respuesta1 : preguntas[0].respuestas[0].respuesta1;
+  let respuesta2 = indicePregunta.respuestas ? indicePregunta.respuestas[1].respuesta2 : preguntas[0].respuestas[1].respuesta2;
+  let respuesta3 = indicePregunta.respuestas ? indicePregunta.respuestas[2].respuesta3 : preguntas[0].respuestas[2].respuesta3;
+  let respuesta4 = indicePregunta.respuestas ? indicePregunta.respuestas[3].respuesta4 : preguntas[0].respuestas[3].respuesta4;
+  
+   
+  console.log('respuesta1: ', respuesta1);
+  console.log('respuesta2: ', respuesta2);
+  console.log('respuesta3: ', respuesta3);
+  console.log('respuesta4: ', respuesta4);
+
   return (
-    <div>Quiz</div>
+    <>
+    {loading == true ? 
+     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column' }}>
+      <h1 style={{ color: '#124076' }}>Pregunta 1:</h1>
+      <Card
+        hoverable
+        style={{
+          width: '90%', // Ancho del 90% del contenedor padre
+          maxWidth: '600px', // Ancho máximo de 600px
+          backgroundColor: '#124076',
+          marginBottom: '20px',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          padding: '20px',
+        }}
+      >
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+          <img alt="example" src={logo} style={{ width: '200px', height: 'auto' }} /> {/* Ancho del 100% y altura automática */}
+        </div>
+        <h3 style={{ wordWrap: 'break-word', color: 'white', textAlign: 'center' }}>{tituloPregunta}</h3> {/* Alineación del texto al centro y máximo de ancho */}
+      </Card>
+      
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '90%', maxWidth: '600px', marginBottom: '20px' }}>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '50%', paddingRight: '10px' }}>
+        <Button type="primary" danger style={{ marginBottom: '20px', width: '100%', whiteSpace: 'normal', fontSize: '14px', overflowWrap: 'break-word' }}>
+          {respuesta1}
+        </Button>
+        <Button type="primary" danger style={{ width: '100%', whiteSpace: 'normal', fontSize: '14px', overflowWrap: 'break-word' }}>
+          {respuesta2}
+        </Button>
+      </div>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', width: '50%', paddingLeft: '10px' }}>
+        <Button type="primary" danger style={{ marginBottom: '20px', width: '100%', whiteSpace: 'normal', fontSize: '14px', overflowWrap: 'break-word' }}>
+          {respuesta3}
+        </Button>
+        <Button type="primary" danger style={{ width: '100%', whiteSpace: 'normal', fontSize: '14px', overflowWrap: 'break-word' }}>
+          {respuesta4}
+        </Button>
+      </div>
+    </div>
+    
+    </div> : <h1>Cargando...</h1>}
+    
+    </>
   )
 }
