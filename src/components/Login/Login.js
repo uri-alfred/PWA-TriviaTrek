83adf413bf5aedd0246dec425eafccba6beb1453
DaveGC97 <88519545+DaveGC97@users.noreply.github.com>
2024-03-20 16:56:21 -0600

    Validación de errores y funcinalidad de login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,41 @@
 ﻿import React, { useState } from 'react';
-import { Button, Form, Input, Typography } from 'antd';
+import { Alert, Button, Form, Input, Typography } from 'antd';
+import {useAuth} from '../../context/authContext';
 
 const { Text } = Typography;
 
 export default function Login() {
   const [form] = Form.useForm();
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
+  const {login} = useAuth();
 
-  const onFinish = (values) => {
+  const onFinish = async (values) => {
     setLoading(true);
-    console.log('Attempting to log in:', values);
-    // Aquí puedes agregar la lógica de autenticación y enviar los datos al servidor
-    // Simulación de respuesta del servidor
+    setError("");
+    try {
+      await login(values.correo,values.contraseña)
+    }
+    catch (error){
+      switch (error.code) {
+        case "auth/user-not-found":
+          setError("No existe un usuario con ese correo.");
+          break;
+        case "auth/invalid-email":
+          setError("Se requiere ingresar un correo.");
+          break;
+        case "auth/wrong-password":
+          setError("La contraseña es invalida.");
+          break;
+        case "auth/internal-error":
+          setError("Se requiere ingresar una contraseña.");
+          break;
+
+        default:
+          setError(error.message);
+          break;
+      }
+    }
     setTimeout(() => {
       setLoading(false);
     }, 2000);
@@ -25,6 +49,12 @@ export default function Login() {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'flex-start', minHeight: '100vh', paddingTop: '50px', backgroundColor: '#f0f2f5' }}>
       <div style={{ maxWidth: 650, width: '100%', padding: 90, paddingTop: 10, paddingBottom: 35, border: '1px solid #ccc', borderRadius: 5, backgroundColor: '#fff' }}>
         <h2 style={{ textAlign: 'center', marginBottom: 20, fontSize: '30px' }}>Inicia Sesión</h2>
+        {error &&
+        <div>
+        <Alert message="Error Text" type="error" />
+        <br />
+        </div>
+        }
         <Form
           form={form}
           name="login"
@@ -44,6 +74,10 @@ export default function Login() {
               {
                 required: true,
                 message: 'Por favor ingresa tu correo',
+              },
+              {
+                type: 'email',
+                message: 'Ingresa un correo electrónico válido',
               },
             ]}
             style={{ marginBottom: '15px' }} // Ajuste para agregar margen inferior al campo
